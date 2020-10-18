95b420d020f5b8da24293a044658736e5ac9af8a
MeLJes <meljesx@gmail.com>
2020-10-18 21:33:56 +0300

    Final

--- js/main.js
+++ js/main.js
@@ -20,6 +20,35 @@
     [...document.getElementsByClassName('dropdown__btn')].forEach(button => {
       button.addEventListener('click', event => button.nextElementSibling.classList.toggle('dropdown__list--inactive'));
     });
+
+    [...document.getElementsByClassName('article__share-btn')].forEach(button => {
+      button.addEventListener('click', event => {
+        button.classList.toggle('article__share-btn--hidden');
+        button.nextElementSibling.classList.toggle('article__shared--hidden');
+      });
+    });
+
+    [...document.getElementsByClassName('user__report')].forEach(button => {
+      button.addEventListener('click', event => {
+        document.querySelector('.modal--report').classList.toggle('modal--hidden');
+      });
+    });
+
+    [...document.getElementsByClassName('user__block')].forEach(button => {
+      button.addEventListener('click', event => {
+        document.querySelector('.modal--block').classList.toggle('modal--hidden');
+      });
+    });
+
+    document.querySelector('.btn-add').addEventListener('click', event => {
+      document.querySelector('.modal--add-article').classList.toggle('modal--hidden');
+    });
+
+    [...document.getElementsByClassName('modal__close')].forEach(button => {
+      button.addEventListener('click', event => {
+        button.closest('.modal').classList.toggle('modal--hidden');
+      });
+    });
   });
 
   // --- Events
